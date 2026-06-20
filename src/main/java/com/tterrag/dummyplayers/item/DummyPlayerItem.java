a8a6cc284f3230cb5c51984db63d76e9c4b48cca
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2026-06-20 08:11:02 -0500

    Update to 26.2

--- src/main/java/com/tterrag/dummyplayers/item/DummyPlayerItem.java
+++ src/main/java/com/tterrag/dummyplayers/item/DummyPlayerItem.java
@@ -14,6 +14,7 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.PostSpawnProcessor;
 import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -49,7 +50,7 @@ public class DummyPlayerItem extends Item {
 
         ItemStack itemStack = context.getItemInHand();
         if (level instanceof ServerLevel serverLevel) {
-            Consumer<DummyPlayerEntity> consumer = EntityType.createDefaultStackConfig(serverLevel, itemStack, context.getPlayer());
+            PostSpawnProcessor<DummyPlayerEntity> consumer = EntityType.createDefaultStackConfig(serverLevel, itemStack, context.getPlayer());
             ResolvableProfile profile = itemStack.get(DataComponents.PROFILE);
             if (profile != null) {
                 consumer = consumer.andThen(dummy -> dummy.setAndFillProfile(profile));
